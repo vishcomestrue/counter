0cef432ae015b7cb27ae5be7f646b521c1e70683
vishcomestrue <ramvishwa03@gmail.com>
2021-09-08 23:11:11 +0530

    Updated Counter component

---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './Counter.css';
+
+const Counter = () => {
+
+  let [count, changeCount] = useState(0)
+
+  return (
+    <div className="localeBody">
+      <h1>C O U N T E R</h1>
+      <h1 id="countNumber">{count}</h1>
+      <div className="buttonEncloser">
+        <button className="button" id="inc" onClick={() => changeCount(count + 1)}>
+          Increase
+        </button>
+        <button className="button" id="dec" onClick={() => changeCount(count - 1)}>
+          Decrease
+        </button>
+      </div>
+      <button className="button" id="reset" onClick={() => changeCount(count = 0)}>
+        Reset
+      </button>
+    </div>
+  )
+}
+
+export default Counter
